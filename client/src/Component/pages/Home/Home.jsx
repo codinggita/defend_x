053b730441d06f7b1c5b9aa3d128945fd5b26430
unknown <s4sudheerbhati2453@gmail.com>
2024-02-03 23:22:45 +0530

    Added The Another Image Slider

--- client/src/Component/pages/Home/Home.jsx
+++ client/src/Component/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import Slider from '../../Slider/Slider'
 import Companies from '../../Companies/Companies'
 import Banner from '../../Banner/Banner'
 import Hero from '../../Hero/Hero'
+import ImageSlider from '../../ImageSlider/ImageSlider'
+
 
 const Home = () => {
   return (
@@ -13,6 +15,7 @@ const Home = () => {
     <Banner />
     <br></br>
     <Hero />
+    <ImageSlider />
     </>
   )
 }
