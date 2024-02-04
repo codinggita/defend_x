ea26ea558a4f489ffd1cc96be125e00ae978f8ac
unknown <s4sudheerbhati2453@gmail.com>
2024-02-04 11:19:58 +0530

    Added the new Team  section on Training Page

--- client/src/Component/pages/Training/Training.jsx
+++ client/src/Component/pages/Training/Training.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Features from './Features/features'
 import Slider from './slide/Slider'
-
+import Team from './Team/Team'
 
 const Training = () => {
   return (
     <>
     <Slider />
     <Features/>
+    <br></br>
+    <Team />
   
     </>
   )
