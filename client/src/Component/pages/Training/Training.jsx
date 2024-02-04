6470e7b5c76a989f0fa73b9bf97bf12fc4886248
unknown <s4sudheerbhati2453@gmail.com>
2024-02-04 11:17:15 +0530

    Added the new slider section on Training Page

--- client/src/Component/pages/Training/Training.jsx
+++ client/src/Component/pages/Training/Training.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Features from './Features/features'
+import Slider from './slide/Slider'
 
 
 const Training = () => {
   return (
     <>
-    
+    <Slider />
     <Features/>
   
     </>
